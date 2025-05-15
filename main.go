c298f03f8b5e69a623f8857fae9b42a7e30be131
Yasyfa Azkaa Wiwaha <yasyfa.azkaa@ui.ac.id>
2025-05-15 18:11:28 +0700

    Init db

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
 )
 
 var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("sqlite3", "./mywebsite.db")
+	if err != nil {
+		// Handle error - perhaps log it and exit or panic
+		panic("failed to open database: " + err.Error())
+	}
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	data := struct{ Greeting string }{Greeting: "Hey Boss!!!"}
@@ -18,6 +29,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	defer db.Close()
+
 	http.HandleFunc("/", handler)
 	fmt.Println("Server listening on port 8080")
 	err := http.ListenAndServe(":8080", nil)
